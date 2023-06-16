7db1185e9ed72e211bca53ab86cfce72836507c1
CarlosSalega <carlosalfredosalega@gmail.com>
2023-06-16 01:48:42 -0200

    2nd commit: theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        DarkGray: "#493843",
+        DarkGreen: "#61988E",
+        LightGreen: "#A0B2A6",
+        Gray: "#CBBFBB",
+        LightGray: "#ECEBF3",
+        Clean: "#EABDA8",
+      },
+      fontFamily: {
+        Manrope: [
+          "https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap",
+        ],
+      },
+    },
   },
   plugins: [],
 };
