0f836f1abc0e4bea8009d8dd6ac9f0a59cef420a
CarlosSalega <carlosalfredosalega@gmail.com>
2023-06-16 01:10:34 -0200

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <main>
+        <h1 className="text-3xl font-bold underline bg-red-500">
+          Advice Generator App
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
