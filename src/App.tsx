808658ae9b8ffd74b7aecfd7d6dbfea78556eaed
CarlosSalega <carlosalfredosalega@gmail.com>
2023-06-19 20:37:05 -0200

    4th commit: basic design in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 function App(): JSX.Element {
   return (
-    <>
-      <main>
-        <h1 className="text-3xl font-bold underline bg-red-500">
-          Advice Generator App
-        </h1>
-      </main>
-    </>
+    <main className="font-Manrope font[28px] bg-Gray min-w-screen min-h-screen flex items-center">
+      <article className="bg-Onyx w-[342px] h-[242px] rounded-xl px-10 pt-10 pb-16 mx-auto text-center relative">
+        <h2 className="text-Glaucous mb-2">
+          Advice #<span>117</span>
+        </h2>
+        <p className="text-Powder">
+          "It's easy to sit up and take notice, what's difficult is gettings up
+          and taking action."
+        </p>
+        <button className="text-Powder cursor-pointer bg-Glaucous px-5 rounded-sm mt-8">
+          Next
+        </button>
+      </article>
+    </main>
   );
 }
 
