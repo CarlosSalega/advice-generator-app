6403625558c447d218e07f0daea384c7e97f4a92
CarlosSalega <carlosalfredosalega@gmail.com>
2023-06-22 16:20:59 -0200

    6th commit: main functionality of the app, covered

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
 import { useEffect, useState } from "react";
 
-function App(): JSX.Element {
-  const [advice, setAdvice] = useState("");
+interface adviceData {
+  slip: {
+    advice: string;
+  };
+}
 
-  const fetchData = async () => {
+function App(): JSX.Element {
+  const [advice, setAdvice] = useState<string>("");
+  const [randomId, setRandomId] = useState<number>(1);
+
+  const fetchData = async (id: number) => {
     try {
-      const response = await fetch(`https://api.adviceslip.com/advice`);
-      const data = await response.json();
-      setAdvice(data.slip.advice);
+      const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
+      const data: adviceData = await response.json();
+      if (response.ok && data && data.slip && data.slip.advice) {
+        setAdvice(data.slip.advice);
+      } else {
+        setAdvice("...");
+      }
     } catch (error) {
       console.error("An error has occurred", error);
     }
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData(randomId);
+  }, [randomId]);
 
-  const handleClick = () => fetchData();
+  const handleClick = () => {
+    let newRandomId = Math.floor(Math.random() * 200);
+    while (newRandomId === 0) {
+      newRandomId = Math.floor(Math.random() * 200);
+    }
+    setRandomId(newRandomId);
+  };
 
   return (
-    <main className="font-Manrope font[28px] bg-Gray min-w-screen min-h-screen flex items-center">
-      <article className="bg-Onyx w-[342px] h-[242px] rounded-xl px-10 pt-10 pb-16 mx-auto text-center relative">
-        <h2 className="text-Glaucous mb-2">
-          Advice #<span>117</span>
+    <main className="font-Manrope bg-Gray min-h-screen flex items-center justify-center">
+      <article className="bg-Onyx w-96 h-96 rounded-xl p-8 flex items-center justify-center flex-col">
+        <h2 className="text-Glaucous mb-4">
+          Tip #<span>{randomId}</span>
         </h2>
-        <p className="text-Powder">{advice}</p>
+        <div className="h-40 flex items-center">
+          <p className="text-Powder text-center">{advice}</p>
+        </div>
         <button
-          className="text-Powder cursor-pointer bg-Glaucous px-5 rounded-sm mt-8"
+          className="text-Powder cursor-pointer bg-Glaucous px-4 py-2 rounded-md mt-4"
           onClick={handleClick}
         >
           Next
