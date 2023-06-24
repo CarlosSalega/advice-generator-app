0d07e4457c000e582a53205d6b4df46348f21404
CarlosSalega <carlosalfredosalega@gmail.com>
2023-06-23 23:27:19 -0200

    9th commit: pre-composited app

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ interface adviceData {
 }
 
 function App(): JSX.Element {
-  const [advice, setAdvice] = useState<string>("");
-  const [randomId, setRandomId] = useState<number>(1);
+  const [advice, setAdvice] = useState<string | JSX.Element>("");
+  const [randomAdviceId, setRandomAdviceId] = useState<number>(1);
 
   const fetchData = async (id: number) => {
     try {
@@ -18,7 +18,7 @@ function App(): JSX.Element {
       if (response.ok && data && data.slip && data.slip.advice) {
         setAdvice(data.slip.advice);
       } else {
-        setAdvice("...");
+        setAdvice(<span className="text-4xl font-bold text-Powder">...</span>);
       }
     } catch (error) {
       console.error("An error has occurred", error);
@@ -26,31 +26,51 @@ function App(): JSX.Element {
   };
 
   useEffect(() => {
-    fetchData(randomId);
-  }, [randomId]);
+    fetchData(randomAdviceId);
+  }, [randomAdviceId]);
+
+  const generateRandomAdviceId = (): number => {
+    const newRandomAdviceId = Math.floor(Math.random() * 200);
+    return newRandomAdviceId === 0
+      ? generateRandomAdviceId()
+      : newRandomAdviceId;
+  };
 
   const handleClick = () => {
-    let newRandomId = Math.floor(Math.random() * 200);
-    while (newRandomId === 0) {
-      newRandomId = Math.floor(Math.random() * 200);
-    }
-    setRandomId(newRandomId);
+    const newRandomId = generateRandomAdviceId();
+    setRandomAdviceId(newRandomId);
   };
 
   return (
-    <main className="font-Manrope bg-Gray min-h-screen flex items-center justify-center">
-      <article className="bg-Onyx w-96 rounded-xl p-8 flex items-center justify-center flex-col h-72">
-        <h2 className="text-Glaucous">
-          Tip #<span>{randomId}</span>
+    <main className="font-Manrope bg-Gray min-h-screen min-w-full flex items-center justify-center">
+      <article className="bg-Onyx w-96 rounded-xl p-8 flex items-center justify-center flex-col h-64">
+        <h2 className="text-Glaucous font-bold">
+          Tip #<span>{randomAdviceId}</span>
         </h2>
         <div className="h-40 flex items-center">
           <p className="text-Powder text-center">{advice}</p>
         </div>
+        <div className="mb-4">
+          <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fillRule="nonzero">
+              <path
+                className="fill-Powder"
+                d="M0 8h122v1H0zM173 8h122v1H173z"
+              />
+              <g transform="translate(138)" className="fill-Powder">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+        </div>
         <button
-          className={`text-Powder cursor-pointer bg-Glaucous px-4 py-2 rounded-md hover:bg-Sha`}
+          type="button"
+          className={`text-Powder cursor-pointer bg-Glaucous px-4 py-1 rounded-md inline-flex items-center gap-x-2 hover:bg-Sha`}
           onClick={handleClick}
         >
           {<RiAiGenerate />}
+          <span>Generate</span>
         </button>
       </article>
     </main>
